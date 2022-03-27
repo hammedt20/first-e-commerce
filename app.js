e0273cace2d8507e005e352db5beb367e5e1b19a
unknown <hammedt20@gmail.com>
2022-03-27 02:33:46 +0100

    second commit

--- app.js
+++ app.js
@@ -2,7 +2,9 @@ const navMenu = document.querySelector('.nav-menu'),
     navToggle = document.querySelector('.nav-toggle'),
     navClose = document.querySelector('.nav-close'),
     navLink = document.querySelectorAll('.nav-link'),
-    header = document.querySelector('.header');
+    header = document.querySelector('.header'),
+    sections = document.querySelectorAll(`#home,#featured,#products,#new`),
+    scrollUp = document.querySelector('#scroll-up');
 
 
 if(navToggle){
@@ -30,3 +32,59 @@ window.addEventListener('scroll', () => {
         header.classList.remove('scroll-header');
     }
 })
+
+window.addEventListener('scroll', () => {
+    if(window.scrollY >= 400){
+        scrollUp.classList.add('show-scroll');
+    }else{
+        scrollUp.classList.remove('show-scroll');
+    }
+})
+
+function scrollActive(){
+    const scrollY = window.pageYOffset;
+
+    sections.forEach(current => {
+        const sectionHeight = current.offsetHeight,
+            sectionTop = current.offsetTop - 58,
+            sectionId = current.getAttribute('id');
+
+        if(scrollY > sectionTop && scrollY <= sectionTop +sectionHeight){
+            document.querySelector(`.nav-menu a[href *= "${sectionId}"]`).classList.add(`active-link`);
+        }else{
+            document.querySelector(`.nav-menu a[href *= "${sectionId}"]`).classList.remove(`active-link`);
+        }
+    })
+}
+
+window.addEventListener('scroll', scrollActive);
+
+
+var testimonialSwiper = new Swiper(".testimonial-swiper", {
+    spaceBetween: 30,
+    loop: 'true',
+
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+  });
+
+var newSwiper = new Swiper(".new-swiper", {
+    spaceBetween: 24,
+    loop: 'true',
+
+    breakpoints: {
+        576: {
+            slidesPerView: 2,
+        },
+        768: {
+            slidesPerView: 3,
+        },
+        1024: {
+            slidesPerView: 4,
+        }
+    }
+  });
+
+
